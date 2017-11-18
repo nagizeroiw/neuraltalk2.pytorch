ab1c7830e882cf18fa1eabfab588da61e9c6d1a1
Vargnatt <kudosblade@gmail.com>
2017-11-18 18:09:19 +0800

    fix

--- eval.py
+++ eval.py
@@ -74,7 +74,7 @@ opt = parser.parse_args()
 
 # Load infos
 with open(opt.infos_path) as f:
-    infos = cPickle.load(f)
+    infos = cPickle.load(f,'rb')
 
 # override and collect parameters
 if len(opt.input_fc_dir) == 0:
