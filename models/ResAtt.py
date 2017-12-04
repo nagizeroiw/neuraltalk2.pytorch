6007bfd2be1f42bc8d22f360d7d3dfda0d8d1043
Vargnatt <kudosblade@gmail.com>
2017-12-04 21:45:11 +0800

    my life sucks

--- models/ResAtt.py
+++ models/ResAtt.py
@@ -4,22 +4,21 @@ import torch.nn.functional as F
 from torch.nn import Parameter
 from torch.autograd import *
 import misc.utils as utils
-
+import subprocess
 from .CaptionModel import CaptionModel
 from .AttModel import AttModel
-
 
 
 class CBN2D(nn.Module):
     def __init__(self, feat_size, momentum=0.1):
         super(CBN2D, self).__init__()
-        self.feat_size = 1
+        self.feat_size = feat_size
         # self.gamma = Parameter(torch.Tensor(self.feat_size))
         # self.beta = Parameter(torch.Tensor(self.feat_size))
-        self.var = Variable(torch.Tensor(self.feat_size).cuda())
-        self.miu = Variable(torch.Tensor(self.feat_size).cuda())
-        # self.register_buffer('var',torch.ones(self.feat_size))
-        # self.register_buffer('miu',torch.zeros(self.feat_size))
+        # self.var = Variable(torch.Tensor(self.feat_size).cuda())
+        # self.miu = Variable(torch.Tensor(self.feat_size).cuda())
+        self.register_buffer('var', torch.ones(self.feat_size))
+        self.register_buffer('miu', torch.zeros(self.feat_size))
         self.eps = 1e-9
         self.momentum = momentum
         # self.reset_param()
@@ -28,11 +27,11 @@ class CBN2D(nn.Module):
         assert(x.dim() == 4)
 
         if not gatta is None:
-            gamma = gatta[:, :self.feat_size]
-            beta = gatta[:, self.feat_size:]
+            gamma = gatta.narrow(1, 0, self.feat_size)
+            beta = gatta¡£narrow(1, self.feat_size, self.feat_size)
         else:
-            gamma = Variable(torch.zeros(x.size()))
-            beta = Variable(torch.zeros(x.size()))
+            gamma = Variable(torch.zeros(x.size(0), self.feat_size))
+            beta = Variable(torch.zeros(x.size(0), self.feat_size))
         if x.is_cuda and not gamma.is_cuda:
             gamma = gamma.cuda()
             beta = beta.cuda()
@@ -40,14 +39,14 @@ class CBN2D(nn.Module):
         x = x.view(x_size[0], x_size[1], -1)
         x = x.permute(0, 2, 1)
         # x = x.view(-1, self.feat_size)
-        tmp_x = x.contiguous().view(-1, self.feat_size)
+        tmp_x = x.contiguous().view(-1, self.feat_size).data
         tmp_mean = torch.mean(tmp_x, 0)
         tmp_var = torch.var(tmp_x, 0)
-        beta=beta.squeeze(2).squeeze(3).expand(x.size())
-        beta=beta.squeeze(2).squeeze(3).expand(x.size())
+        gamma = gamma.unsqueeze(1).expand(x.size())
+        beta = beta.unsqueeze(1).expand(x.size())
 
-        out = (x - self.miu) / torch.sqrt(self.var +
-                                          self.eps) * (gamma + 1) + (beta)
+        out = (x - Variable(self.miu)) / torch.sqrt(Variable(self.var) +
+                                                    self.eps) * (gamma + 1) + (beta)
         out = out.permute(0, 2, 1).contiguous()
         out = out.view(x_size)
 
@@ -79,6 +78,10 @@ class ResBlk(nn.Module):
         self.bn1 = CBN2D(opt.rnn_size)
         self.conv3 = nn.Conv2d(opt.rnn_size, opt.rnn_size, 3, padding=1)
         self.bn2 = CBN2D(opt.rnn_size)
+        # self.alpha_beta1 = nn.Sequential(
+        #     nn.Linear(opt.rnn_size, opt.rnn_size), nn.ReLU(), nn.Linear(opt.rnn_size, 2))
+        # self.alpha_beta2 = nn.Sequential(
+        #     nn.Linear(opt.rnn_size, opt.rnn_size), nn.ReLU(), nn.Linear(opt.rnn_size, 2))
         self.alpha_beta1 = nn.Sequential(
             nn.Linear(opt.rnn_size, opt.rnn_size), nn.ReLU(), nn.Linear(opt.rnn_size, 2))
         self.alpha_beta2 = nn.Sequential(
@@ -119,8 +122,7 @@ class ResSeq(nn.Module):
 class ResCore(nn.Module):
     def __init__(self, opt):
         super(ResCore, self).__init__()
-        self.lstm = nn.LSTMCell(
-            opt.input_encoding_size + opt.rnn_size, opt.rnn_size)
+        self.lstm = ResLSTM(opt, opt.input_encoding_size + opt.rnn_size)
         self.resblocks = ResSeq(opt)
         self.prev_out = None
 
@@ -132,14 +134,56 @@ class ResCore(nn.Module):
 
         conv_x = att_feats.permute(0, 3, 1, 2)
         # print(conv_x.size())
-        lstm_input = self.resblocks(conv_x, self.prev_out)
-        out, state = self.lstm(torch.cat((lstm_input, fc_feats), 1), state)
-        # print(out.size())
+        # print(state[0].size())
+        conved_input = self.resblocks(conv_x, self.prev_out)
+        lstm_input = torch.cat((conved_input, xt), 1)
+        # print(state[0].size())
+        out, hidden = self.lstm(lstm_input, state)
+        # print(state.size())
         self.prev_out = out.squeeze()
+        result = subprocess.run(['nvidia-smi'], stdout=subprocess.PIPE)
+        print(result.stdout.decode('utf-8'))
+        # state = (out, hidden)
+        # if state[0].dim() != 3:
+        #     state[0] = state[0].unsqueeze(0)
+        #     state[1] = state[1].unsqueeze(0)
         return out, state
+
+
+class ResLSTM(nn.Module):
+    def __init__(self, opt, input_size):
+        super(ResLSTM, self).__init__()
+        self.rnn_size = opt.rnn_size
+        self.input_size = input_size
+        self.drop_prob_lm = opt.drop_prob_lm
+        self.i2h = nn.Linear(self.input_size, 4 * self.rnn_size)
+        self.dropout = nn.Dropout(self.drop_prob_lm)
+        self.h2h = nn.Linear(self.rnn_size, 4 * self.rnn_size)
+
+    def forward(self, input_var, state):
+        input_sum = self.i2h(input_var) + self.h2h(state[0][-1])
+        # print(input_sum.size())
+        sigmoid_chunk = input_sum.narrow(1, 0, self.rnn_size * 3)
+        sigmoid_chunk = F.sigmoid(sigmoid_chunk)
+        in_gate = sigmoid_chunk.narrow(1, 0, self.rnn_size)
+        out_gate = sigmoid_chunk.narrow(1, self.rnn_size, self.rnn_size)
+        forget_gate = sigmoid_chunk.narrow(1, self.rnn_size * 2, self.rnn_size)
+        in_transform = input_sum.narrow(
+            1, self.rnn_size * 3, self.rnn_size)
+        next_c = forget_gate * state[1][-1] + in_gate * in_transform
+        next_h = out_gate * F.tanh(next_c)
+        output = self.dropout(next_h)
+        state = (next_h.unsqueeze(0), next_c.unsqueeze(0))
+        return output, state
 
 
 class ResModel(AttModel):
     def __init__(self, opt):
         super(ResModel, self).__init__(opt)
         self.core = ResCore(opt)
+
+    # def init_hidden(self, bsz):
+    #     weight = next(self.parameters()).data
+    #     # print(weight)
+    #     return (Variable(weight.new(bsz, self.rnn_size).zero_()),
+    #             Variable(weight.new(bsz, self.rnn_size).zero_()))
